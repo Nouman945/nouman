bb33e4f6dc18f00e2b15e0b297567ff1725b21c2
Abdulrahman10a <alahmed.abdulrahman1@gmail.com>
2023-08-01 08:57:20 +0300

    first commit

---
+++ src/components/icons/logo.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const IconLogo = () => (
+  <svg id="logo" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
+    <title>Logo</title>
+    <g transform="translate(-8.000000, -2.000000)">
+      <g transform="translate(11.000000, 5.000000)">
+        <polygon
+          id="Shape"
+          stroke="currentColor"
+          strokeWidth="5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          points="39 0 0 22 0 67 39 90 78 68 78 23"
+        />
+        <path
+          d="M 4.5 35 L 0 35 L 0 0 L 6.3 0 L 21.75 29.1 L 21.75 0 L 26.25 0 L 26.25 35 L 20.05 35 L 4.5 6.1 L 4.5 35 Z"
+          fill="currentColor"
+        />
+      </g>
+    </g>
+  </svg>
+);
+
+export default IconLogo;
